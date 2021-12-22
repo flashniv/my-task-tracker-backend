c0d4cdfc86c63fd139c44b905d205a3ded425bab
flashniv <flashniv@gmail.com>
2021-12-22 10:50:26 +0200

    initial commit

---
+++ src/main/java/ua/com/serverhelp/mytasktracking/MyTaskTrackingApplication.java
@@ -0,0 +1,13 @@
+package ua.com.serverhelp.mytasktracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTaskTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTaskTrackingApplication.class, args);
+	}
+
+}
