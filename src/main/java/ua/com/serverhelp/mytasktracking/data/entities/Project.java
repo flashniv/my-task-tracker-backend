12ec17bba3eada28486e9b34bd08f04c2fb83c1c
flashniv <flashniv@gmail.com>
2022-01-04 12:48:11 +0200

    Delete Team temporary

--- src/main/java/ua/com/serverhelp/mytasktracking/data/entities/Project.java
+++ src/main/java/ua/com/serverhelp/mytasktracking/data/entities/Project.java
@@ -3,8 +3,6 @@ package ua.com.serverhelp.mytasktracking.data.entities;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Data
@@ -16,11 +14,4 @@ public class Project {
     @ManyToOne
     @JoinColumn(name="organization_id", nullable=false)
     private Organization organization;
-    @ManyToMany
-    @JoinTable(
-            name = "team_project",
-            joinColumns = { @JoinColumn(name = "team_id") },
-            inverseJoinColumns = { @JoinColumn(name = "project_id") }
-    )
-    private List<Team> teams = new ArrayList<>();
 }
